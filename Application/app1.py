d8a8a0ed165cef7aba9fdbf5736ee5b124529940
Ievgen <yevgens@web.de>
2017-09-15 16:15:48 +0300

    the app is more interactive now: added guessing and "y/n" choosing, if the program guessed the word correct.

--- Application/app1.py
+++ Application/app1.py
@@ -3,15 +3,21 @@
 # the user ask to the program a word to explain it.
 
 import json
-import difflib
-
+from difflib import get_close_matches
 
 data = json.load(open("data.json", 'r'))
 
 
 def voc(word):
+    guess = get_close_matches(word, data.keys())[0]
     if word in data:
         return data[word]
+    elif len(get_close_matches(word, data.keys())) > 0:
+        c = input("Did you meant %s instead?" % guess + " press y/n: ")
+        if c == "y":
+            return data[guess]
+        else:
+            return "The word doesn't exist. Please double check it!"
     else:
         return "The word doesn't exist. Please double check it!"
 
