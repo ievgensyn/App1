4d5bc0b2c5b23abd7daa3c2c7e58b2b058d7841c
Ievgen <yevgens@web.de>
2017-09-16 14:36:28 +0300

    more functionality, more user-friendly for eyes output

--- Application/app1.py
+++ Application/app1.py
@@ -7,22 +7,47 @@ from difflib import get_close_matches
 
 data = json.load(open("data.json", 'r'))
 
+
 def voc(w):
-    lst = get_close_matches(w, data.keys())
+    lst = get_close_matches(w, data.keys(), cutoff=0.8)
     if w in data:
         return data[w]
     elif len(lst) > 0:
         c = input("Did you mean %s instead?" % lst[0] + " press y/n: ")
+        while len(c) > 1 or c.lower() != "y" and c.lower() != "n":
+            c = input("Your entry is invalid. Please enter the letter 'y' for 'yes' or 'n' for 'no': "
+            ).lower()
         if c == "y":
             return data[lst[0]]
         elif c == "n":
-            return "The word doesn't exist. Please double check it!"
+            dic = dict(enumerate(lst, start=1))
+            print(dic)
+            try:
+                d = int(input("Which word did you want to translate?\nPlease, enter the number corresponded to the word: "))
+                if d in dic:
+                    return data[dic[d]]
+                else:
+                    return "The number you're entering isn't correct."
+            except ValueError as error:
+                print(error)
+
         else:
-            return "Sorry, we didn't understand your entry."
+            return "The word doesn't exist. Please double check it!"
     else:
         return "The word doesn't exist. Please double check it!"
 
 
-word = input("Enter word: ").lower()
-
-print(voc(word))
+while True:
+    word = input("Enter word ('q' for quit): ").lower()
+    if word == "q":
+        print("See you again!")
+        break
+    else:
+        result = voc(word)
+        if type(result) == list:
+            count = 0
+            for i in result:
+                count += 1
+                print(str(count) + ". " + i)
+        else:
+            print(voc(word))
